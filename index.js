9a8668af4596a999d1219a50e4491dc963e2f3d9
RAJ KUMAR SEN <rajkumarshrivash374@gmail.com>
2023-03-01 01:38:34 +0530

    experience section completed

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
 var icon = document.getElementById("light-dark-icon");
 var logo = document.getElementById("logo");
 
-icon.onclick = function(){
+icon.onclick = function () {
     document.body.classList.toggle("dark-mode");
-    if(document.body.classList.contains("dark-mode")){
+    if (document.body.classList.contains("dark-mode")) {
         icon.src = "./logoAndImgaes/dark-mode.png";
         logo.src = "./logoAndImgaes/Portfolio_logo.png";
     }
@@ -19,56 +19,54 @@ icon.onclick = function(){
 
 // *************************************************************************************************
 
-// *********************SIdebar icon menu operner************************************
+// *********************Sidebar icon menu opener************************************
 
 let sidebar = document.getElementById("sidebar");
 
 function myFunction(x) {
 
     x.classList.toggle("change");
-    if(x.classList.contains("change")){
+    if (x.classList.contains("change")) {
         sidebar.style.visibility = "visible";
         sidebar.style.animation = "sidebar-load 400ms ease-in";
-
-
-    }else {
+    } else {
         sidebar.style.animation = "sidebar-load 400ms ease-out";
         sidebar.style.visibility = "hidden";
 
     }
-  }
+}
 
 //   *******************************************************************************************
 
-  //********************* */ back to top button functionality********************************
+//********************* */ back to top button functionality********************************
 
-  let btn = document.querySelector("#backToTop");
-  window.addEventListener("scroll", () => {
+let btn = document.querySelector("#backToTop");
+window.addEventListener("scroll", () => {
 
-      if (window.scrollY > 200) {
-          btn.classList.add("active");
-      } else {
-          btn.classList.remove("active");
-      }
-  })
+    if (window.scrollY > 200) {
+        btn.classList.add("active");
+    } else {
+        btn.classList.remove("active");
+    }
+})
 
-  const backToTop = () => {
-      document.body.scrollTop = 0;  // For Safari
-      document.documentElement.scrollTop = 0;  // For Chrome, Firefox, IE and Opera
-      //  ( documentElement  or  scrollingElement )
-  }
+const backToTop = () => {
+    document.body.scrollTop = 0;  // For Safari
+    document.documentElement.scrollTop = 0;  // For Chrome, Firefox, IE and Opera
+    //  ( documentElement  or  scrollingElement )
+}
 
-  // ******************************************************************************************
+// ******************************************************************************************
 
 //*********************************side nav bar active inactive************************************************ */
 
 let sidebarActive = document.querySelector("#sidebar-nav-menu-box ul li");
 
-sidebarActive.onclick = ()=>{
-    if(!sidebarActive.classList.contains("active")){
+sidebarActive.onclick = () => {
+    if (!sidebarActive.classList.contains("active")) {
         sidebarActive.classList.add("active");
     }
-    else{
+    else {
         sidebarActive.classList.remove("active")
     }
 }
